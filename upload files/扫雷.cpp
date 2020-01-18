62b14591754156002d7b82fb7bc4168e7c19e322
LazyBonesMembers <48169674+LazyBonesMembers@users.noreply.github.com>
2020-01-18 13:21:50 +0800

    Add files via upload

---
+++ upload files/扫雷.cpp
@@ -0,0 +1,321 @@
+﻿//#include"pch.h"
+#include<iostream>
+#include<stdlib.h>
+#include<ctime>
+#include<cstdio>
+#include<cmath>
+#include<iomanip>
+#include<cstring>
+#include<Windows.h>
+
+#define N 3 //用来控制屏幕居中值
+
+using namespace std;
+
+int choose1,choose2;//用户选择的难度
+int final_score = 0;//总分
+int s = 1;//局数
+char a[35][35];//定义全局棋盘，先全部定义为未选状态。即下划线：95。这个是在游戏结束后给用户看的
+char b[35][35];//这个是用户在游戏上实时看到的界面，也全部定义为下划线。
+int rest;//剩下的小旗数
+
+struct result{//记录成绩
+	float t;
+	int difficulty;
+	int size;
+	bool win;
+}res[20];
+
+void welcome()//欢迎以及选择难度。
+{
+	cout << endl << endl << endl << endl;
+	cout << "                                          " << "欢迎来到扫雷游戏" << endl << endl;
+	cout << "                                  " << "作者：广州外国语学校编程社茹法锐" << endl << endl;
+	cout << "                                            " << "请输入两个数选择游戏难度：" << endl;
+	cout << "                                             " << "1.20个地雷" << endl;
+	cout << "                                             " << "2.40个地雷" << endl;
+	cout << "                                             " << "3.90个地雷" << endl;
+	cout << endl;
+	cout << "                                             " << "1.10x10大小" << endl;
+	cout << "                                             " << "2.20x20大小" << endl;
+	cout << "                                             " << "3.30x30大小" << endl;
+	cout << endl;
+
+	LOOP1:cin >> choose1>>choose2;
+
+	if (choose1 < 0 || choose2 < 0 || choose1>3 || choose2>3)
+	{
+		cout << "输入错误，请输入两个1~3的整数，用空格隔开" << endl;
+		goto LOOP1;
+	}
+
+	system("cls");//清屏
+
+	switch (choose1)//将用户选择的难度转化为雷数
+	{
+		case 1:
+			res[s].difficulty = 20;
+			break;
+		case 2:
+			res[s].difficulty = 40;
+			break;
+		case 3:
+			res[s].difficulty = 90;
+			break;
+	}
+	rest = res[s].difficulty;//将雷数赋值给剩余雷数
+
+	res[s].size = choose2 * choose2 * 100;//整个棋盘的大小
+
+	return;
+}
+
+void tell_rule()
+{
+	cout << endl << endl << endl << endl;
+	cout << "                                             " << "请输入三个字符来表示您的操作" << endl << endl;
+	cout << "                                       " << "第一个字符：要操作位置的横坐标（数字）" << endl<<endl;
+	cout << "                                     " << "第二个字符：要操作位置的纵坐标（小写字母）" << endl << endl;
+	cout << "                                 " << "第三个字符：操作类型：1.踩下  2.插小旗 3.拔小旗" << endl << endl;
+	system("pause");
+	system("cls");
+}
+
+void build()//将雷生成，并写入二维数组 a
+{
+	for (int i = 0; i < choose2 * 10; i++)
+	{
+		for (int j = 0; j < choose2 * 10; j++)
+		{
+			a[i][j] = 95;
+			b[i][j] = 95;
+		}
+	}
+
+	srand((unsigned)time(NULL));//初始化时间种子
+
+	int m, n;
+	for (int i = 0; i < res[s].difficulty; i++)
+	{
+		
+		
+		m = rand() % choose2*10;//横坐标
+		n = rand() % choose2*10;//纵坐标
+
+		while (1)
+		{
+			if (a[m][n] != 79)
+			{
+				a[m][n] = 79;//79为大写字母O
+				break;//如果和之前的没有重复，直接写入
+			}
+
+			int temp = rand() % 4;//否则的话用随机数看看上下左右选哪个好
+			switch (temp)
+			{
+			case 0:
+				m++;
+				if (m > choose2 * 10)
+				{
+					m -= 4;
+				}
+				break;
+			case 1:
+				m--;
+				if (m < 0)
+				{
+					m += 4;
+				}
+				break;
+			case 2:
+				n++;
+				if (n > choose2 * 10)
+				{
+					n -= 4;
+				}
+				break;
+			case 3:
+				n--;
+				if (n < 0)
+				{
+					n += 4;
+				}
+				break;
+			}
+		}
+	}
+	return;
+}
+
+void show_b()//用户在使用时每输入一次就刷新一次二维数组 b。
+{
+	int p = 0;//位于屏幕上方的数字坐标
+	char q = 97;//位于屏幕左边的字母坐标
+	cout << "   ";
+	for (p = 0; p < choose2 * 10; p++)//输出上坐标
+		cout << setw(N) << p;
+	cout << endl;
+
+	for (int i = 0; i < choose2 * 10; i++)
+	{
+		cout << setw(N) << q;
+		q++;
+		for (int j = 0; j < choose2 * 10; j++)
+		{
+			cout << setw(N) << b[j][i];
+		}
+		cout << endl;
+	}
+	cout << endl<<"剩余小旗数" << rest << endl<<endl;
+}
+
+void lose()
+{
+
+	int p = 0;//位于屏幕上方的数字坐标
+	char q = 97;//位于屏幕左边的字母坐标
+	cout << "   ";
+	for (p = 0; p < choose2 * 10; p++)//输出上坐标
+		cout << setw(N) << p;
+	cout << endl;
+
+	for (int i = 0; i < choose2 * 10; i++)
+	{
+		cout << setw(N) << q;
+		q++;
+		for (int j = 0; j < choose2 * 10; j++)
+		{
+			if (b[j][i] == 33)//用户标了雷
+			{
+				if (a[j][i] == 95)//没有雷
+				{
+					cout << setw(N) << "X";
+					continue;
+				}
+				if (a[j][i] == 79)//有雷
+				{
+					cout << setw(N) << "O";
+					continue;
+				}
+			}
+			cout << setw(N) << a[j][i];
+		}
+		cout << endl;
+	}
+	system("pause");//写到这里
+	return;
+}
+
+void ask()
+{
+	int x,type;
+	char y;
+	cout << endl;
+	while (1)//一直问用户直到得到合法输入
+	{
+		LOOP2:cout << "请输入:" << endl;
+		cin >> x >> y >> type;
+		y -= 'a';//将用户输入的字母转换为数字
+		if (x<0 || x>=choose2 * 10 || (int)y < 0 || (int)y >= choose2 * 10)//输入压根不合法
+		{
+			cout << "输入非法" << endl;
+			cout << "请输入三个字符来表示您的操作" << endl;
+			cout << "第一个字符：要操作位置的横坐标（数字）" << endl;
+			cout << "第二个字符：要操作位置的纵坐标（小写字母）" << endl;
+			cout << "第三个字符：操作类型：1.踩下  2.插小旗 3.拔小旗" << endl;
+			system("pause");
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	if (type == 1)//踩下
+	{
+		if (b[x][y] == 33)//标了雷
+			rest++;
+
+		if (a[x][y] == 79)//有雷
+		{
+			system("cls");
+			cout << "you lose!" << endl;
+			Sleep(5000);
+			system("cls");
+			lose();
+		}
+
+		int sum = 0;//周边的雷数,有越界bug
+		if (a[x + 1][y] == 79)
+			sum++;
+		if (a[x - 1][y] == 79)
+			sum++;
+		if (a[x][y + 1] == 79)
+			sum++;
+		if (a[x][y - 1] == 79)
+			sum++;
+		if (a[x + 1][y + 1] == 79)
+			sum++;
+		if (a[x + 1][y - 1] == 79)
+			sum++;
+		if (a[x - 1][y + 1] == 79)
+			sum++;
+		if (a[x - 1][y - 1] == 79)
+			sum++;
+
+		b[x][y] = sum + 48;//将下划线变为数字
+		return;
+	}
+
+	if (type == 2)//插小旗
+	{
+		if (b[x][(int)y] != 95)//不是未知
+		{
+			goto LOOP2;//去提示错误那里
+		}
+		else
+		{
+			b[x][(int)y] = 33;//将下划线变为感叹号
+			rest--;//剩余小旗数减一
+		}
+		return;
+	}
+
+	if (type == 3)//拔小旗
+	{
+		if (b[x][y] != 33)//不是小旗
+		{
+
+			goto LOOP2;
+		}
+		else
+		{
+			b[x][y] = 95;//将感叹号变回下划线
+			rest++;//剩余小旗数加一
+		}
+	}
+	return;
+}
+
+int main()//主函数
+{
+	system("mode con cols=300 lines=40");//改变宽高
+	system("color 0f");//改变颜色
+	cout << (char)182;
+	while (1)
+	{
+		s++;//局数加一
+		welcome();//欢迎以及选择难度。
+		tell_rule();//输出游戏规则。
+		build();//随机生成地雷。
+		
+		while (1)
+		{
+			show_b();
+
+			ask();
+
+			system("cls");
+		}
+	}
+}
---
+++ upload files/贪吃蛇.cpp
@@ -0,0 +1,373 @@
+﻿//#include"pch.h"//可以被vs2017编译通过
+#include <stdlib.h>  //srand函数的库
+#include <conio.h>  //_getch函数
+#include <time.h>  //time函数的库（用于生成随机数）
+#include <iostream>  //基本输入输出
+
+#define  N 22  //使用一个全局定量来规定游戏区的大小，是值得学习的做法。因为这样方便随时修改。
+
+using namespace std;
+
+
+//以下是全局变量：
+int gameover;//输赢状态
+
+int food_x, food_y; // 随机出米  
+
+int x, y;//这是蛇头的坐标
+
+long start;
+
+//=======================================  
+//类的实现与应用initialize(初始化）
+//=======================================  
+
+//下面定义贪吃蛇的坐标类  
+class snake_position
+{
+public://必须定义为公用
+
+	int x, y;//x表示行，y表示列  
+
+	snake_position() {};
+
+	void initialize(int &);//坐标初始化  
+
+};
+
+snake_position position[(N - 2)*(N - 2) + 1]; //定义贪吃蛇坐标类数组，有(N-2)*(N-2)个坐标  
+
+void snake_position::initialize(int &j)//也是初始化
+{
+	x = 1;
+	y = j;
+}
+
+
+//下面定义贪吃蛇的棋盘图  
+
+class snake_map
+{
+private:
+
+	char s[N][N];//定义贪吃蛇棋盘，包括墙壁。
+
+	int grade, length;//级数和长度
+
+	int gamespeed; //前进时间间隔  
+
+	char direction; // 初始情况下，向右运动  
+
+	int head, tail;
+
+	int score;//分数
+
+	bool gameauto;//是否选择自动升级模式
+
+public:
+
+	snake_map(int h = 4, int t = 1, int l = 4, char d = 77, int s = 0) :length(l), direction(d), head(h), tail(t), score(s) {}
+
+	void initialize();   //声明函数  
+
+	void show_game();
+
+	int updata_game();
+
+	void setpoint();
+
+	void getgrade();
+
+	void display();
+
+
+};
+
+//定义初始化函数，将贪吃蛇的棋盘图进行初始化  
+
+void snake_map::initialize()
+{
+	int i, j;
+
+	for (i = 1; i <= 3; i++)
+
+		s[1][i] = '*';
+
+	s[1][4] = '#';
+
+	for (i = 1; i <= N - 2; i++)
+
+		for (j = 1; j <= N - 2; j++)
+
+			s[i][j] = ' '; // 初始化贪吃蛇棋盘中间空白部分  （先全部设为空白）
+
+	for (i = 0; i <= N - 1; i++)
+
+		s[0][i] = s[N - 1][i] = '-'; //初始化贪吃蛇棋盘上下墙壁  
+
+	for (i = 1; i <= N - 2; i++)
+
+		s[i][0] = s[i][N - 1] = '|'; //初始化贪吃蛇棋盘左右墙壁  
+}
+
+
+//============================================  
+//输出贪吃蛇棋盘信息  
+
+void snake_map::show_game()//输出函数
+{
+
+	system("cls"); // 清屏  
+
+	int i, j;//二维数组输出
+
+	cout << endl;
+
+	for (i = 0; i < N; i++)
+	{
+
+		cout << '\t';//先空一行
+
+		for (j = 0; j < N; j++)
+		{
+			cout << s[i][j] << ' '; // 输出贪吃蛇的全部内容
+		}
+		//下面是在右边的提示
+		if (i == 2) cout << "\t等级：" << grade;
+		if (i == 6) cout << "\t速度：" << gamespeed;
+		if (i == 10) cout << "\t得分：" << score << "分";
+		if (i == 14) cout << "\t暂停：按一下空格键";
+		if (i == 18) cout << "\t继续：按两下空格键";
+
+		cout << endl;
+	}
+}
+
+
+void snake_map::getgrade()//输入选择等级  
+{
+	cin >> grade;
+
+	while (grade > 7 || grade < 1)//如果遇到非法输入
+	{
+		cout << "请输入数字1-7选择等级，输入其他数字无效" << endl;
+
+		cin >> grade;
+	}
+	switch (grade)
+	{
+	case 1: gamespeed = 1000; gameauto = 0; break;
+
+	case 2: gamespeed = 800; gameauto = 0; break;
+
+	case 3: gamespeed = 600; gameauto = 0; break;
+
+	case 4: gamespeed = 400; gameauto = 0; break;
+
+	case 5: gamespeed = 200; gameauto = 0; break;
+
+	case 6: gamespeed = 100; gameauto = 0; break;
+
+	case 7: grade = 1; gamespeed = 1000; gameauto = 1; break;
+
+	}
+}
+
+
+void snake_map::display()//输出等级，得分情况以及称号  
+{
+
+	cout << "\n\t\t\t\t等级：" << grade;
+
+	cout << "\n\n\n\t\t\t\t速度：" << gamespeed;
+
+	cout << "\n\n\n\t\t\t\t得分：" << score << "分";
+
+}
+
+
+void snake_map::setpoint()//随机产生米  
+{
+	srand((unsigned)time(NULL));
+
+	do{
+
+		food_x = rand() % (N - 2) + 1;
+
+		food_y = rand() % (N - 2) + 1;
+
+	} while (s[food_x][food_y] != ' ');
+
+	s[food_x][food_y] = '*';
+}
+
+char key;
+
+int snake_map::updata_game()
+{
+	gameover = 1;
+
+	key = direction;
+
+	start = clock();
+
+	while ((gameover = (clock() - start <= gamespeed)) && !_kbhit());//如果有键按下或时间超过自动前进时间间隔则终止循环  
+
+
+
+	if (gameover)
+	{
+
+		_getch();
+
+		key = _getch();
+	}
+
+	if (key == ' ')
+	{
+		while (_getch() != ' ') {};//这里实现的是按空格键暂停，按空格键继续的功能，但不知为何原因，需要按两下空格才能继续。这是个bug。  
+	}
+	else//排除其他情况，就是方向键
+	{
+		direction = key;
+	}
+
+	switch (direction)//按照上下左右的指示改变蛇头方向
+	{
+		case 72: x = position[head].x - 1; y = position[head].y; break; // 向上  
+				
+		case 80: x = position[head].x + 1; y = position[head].y; break; // 向下  
+
+		case 75: x = position[head].x; y = position[head].y - 1; break; // 向左  
+
+		case 77: x = position[head].x; y = position[head].y + 1; break; // 向右  
+
+		default:gameover = 0;//不是方向键，即非法输入，那么你输了。
+	}
+
+	
+	if (x == 0 || x == N - 1 || y == 0 || y == N - 1)   // 碰到墙壁  
+	{
+		gameover = 0;//那么你输了
+	}
+	else if (s[x][y] != ' ' && !(x == food_x && y == food_y))    // 蛇头碰到蛇身  
+	{
+		gameover = 0;//那么你输了
+	}
+	else if (x == food_x && y == food_y)// 吃米，长度加1  
+	{ 
+		length++;//长度加一
+
+		if (length >= 8 && gameauto)//如果是自动升级
+
+		{
+
+			length -= 8;
+
+			grade++;
+
+			if (gamespeed >= 200)//如果没有升到顶级
+				gamespeed -= 200; // 改变贪吃蛇前进速度  
+			else
+				gamespeed = 100;//最高速，不能再快了
+
+		}
+
+		s[x][y] = '#';  //更新蛇头  
+
+		s[position[head].x][position[head].y] = '*'; //吃米后将原先蛇头变为蛇身  
+
+		head = (head + 1) % ((N - 2)*(N - 2));   //取蛇头坐标  
+
+		position[head].x = x;//更新蛇头横坐标
+
+		position[head].y = y;//更新蛇头纵坐标
+
+		show_game();//调用输出函数，输出新的内容到屏幕
+
+		gameover = 1;
+
+		score += grade * 20;  //加分  
+
+		setpoint();   //新产生米  
+
+	}
+	else// 没吃到米  
+	{ 
+		s[position[tail].x][position[tail].y] = ' ';//将蛇尾置空  
+
+		tail = (tail + 1) % ((N - 2) * (N - 2));//更新蛇尾坐标  
+
+		s[position[head].x][position[head].y] = '*';  //将蛇头更为蛇身  
+
+		head = (head + 1) % ((N - 2) * (N - 2));
+
+		position[head].x = x;//更新蛇头横坐标
+
+		position[head].y = y;//更新蛇头纵坐标
+
+		s[position[head].x][position[head].y] = '#'; //更新蛇头  
+
+		gameover = 1;//没输
+
+	}
+
+	return gameover;//返回gameover的值，判断是否输了
+}
+//====================================  
+//主函数部分  
+//====================================  
+int main()
+{
+	char ctn = 'y';//用来判断是否要再来一局
+
+	int nodead;
+
+	cout << "\n\n\n\n\n\t\t\t 欢迎进入贪吃蛇游戏!" << endl;//欢迎界面;  
+
+	cout << "\n\n\n\t\t\t 按任意键马上开始……" << endl;//准备开始;;  
+
+	_getch();//输入一个任意键
+
+	while (ctn == 'y')//主循环，如果一直“再来一局”就一直执行下去
+	{
+		system("cls"); // 清屏  
+
+		snake_map snake;//生成类
+
+		snake.initialize();//调用初始化函数
+
+		cout << "\n\n请输入数字选择游戏等级：" << endl;
+		cout << "\n\n\n\t\t\t1.等级一：速度 1000 \n\n\t\t\t2.等级二：速度 800 \n\n\t\t\t3.等级三：速度 600 ";
+		cout << "\n\n\t\t\t4.等级四：速度 400 \n\n\t\t\t5.等级五：速度 200 \n\n\t\t\t6.等级六：速度 100 \n\n\t\t\t7.自动升级模式" << endl;
+		snake.getgrade();//获取等级  
+
+		for (int i = 1; i <= 4; i++)
+		{
+			position[i].initialize(i);//初始化坐标  
+		}
+
+		snake.setpoint();  // 产生第一个米  
+
+		do{
+			snake.show_game();
+
+			nodead = snake.updata_game();
+
+		} while (nodead);
+
+		system("cls"); //清屏  
+
+		cout << "\n\n\n\t\t\t\tGameover！\n\n" << endl;
+
+		snake.display();//输出等级/得分情况  
+
+		cout << "\n\n\n\t\t    是否选择继续游戏？输入 y 继续，n 退出" << endl;
+
+		cin >> ctn;
+
+	}
+
+	return 0;//程序结束
+}
+
